53f8c69eeae45b1ccb5e2993519cf6fa42022369
PhuVinhAI <vinhckid5@gmail.com>
2026-07-04 15:30:10 +0700

    refactor: update demo APK URL and filename in config

--- landing/src/config.ts
+++ landing/src/config.ts
@@ -1,5 +1,5 @@
 export const DEMO_APK_URL =
-	'https://github.com/PhuVinhAI/LinVNix/releases/download/v0.0.03062026/LinVNix-demo-0.0.03062026.apk';
-export const DEMO_APK_FILENAME = 'LinVNix-demo-0.0.03062026.apk';
+	'https://github.com/PhuVinhAI/LinVNix/releases/download/BaoCao/linvnix-baocao.apk';
+export const DEMO_APK_FILENAME = 'linvnix-baocao.apk';
 export const CONTACT_URL = 'https://zalo.me/0762605309';
 export const ADMIN_URL = 'https://linvnix-admin.pages.dev';
